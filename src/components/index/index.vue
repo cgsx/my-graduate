7868f8dd2fa3c083028bb8aa896d899ef6523198
Administrator <767086748@qq.com>
2018-02-25 11:26:52 +0800

    框架搭建

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,8 +1,195 @@
 <template>
-  <p>
-    <video src="../../assets/images/banner.mp4" width="100%" loop autoplay></video>
-    <span>zheshishouye</span>
-  </p>
+  <div>
+    <p>
+      <video src="../../assets/images/banner.mp4" width="100%" loop autoplay></video>
+    </p>
+   <Row>
+     <Col span="24" class="productTitle ">
+     <h2>行业智能决策产品</h2>
+     <img src="../../assets/images/tips_img_M.png" height="11" width="99"/>
+     <p>在智能认知基础上对复杂业务问题进行自动识别和判断，结合行业特点，实现业务路径自动选择的智能产品体系。</p>
+     </Col>
+   </Row>
+    <Row>
+     <Col span="20" push="2" class="fle mb30">
+        <div class="decision ">
+          <img src="../../assets/images/0dd34c5c-078a-4edc-a983-af8554dcb333.png" />
+          <h3>
+            DeepGovernor <br>
+            智能政府决策系统
+          </h3>
+          <p>
+            基于复杂决策模型，进行态势感知、预测分析、仿真模拟等，实现对决策业务事前判断、事中控制、事后反馈的全局智能支持系统。
+          </p>
+        </div>      <div class="decision ">
+          <img src="../../assets/images/0dd34c5c-078a-4edc-a983-af8554dcb333.png" />
+          <h3>
+            DeepGovernor <br>
+            智能政府决策系统
+          </h3>
+          <p>
+            基于复杂决策模型，进行态势感知、预测分析、仿真模拟等，实现对决策业务事前判断、事中控制、事后反馈的全局智能支持系统。
+          </p>
+        </div>      <div class="decision ">
+          <img src="../../assets/images/0dd34c5c-078a-4edc-a983-af8554dcb333.png" />
+          <h3>
+            DeepGovernor <br>
+            智能政府决策系统
+          </h3>
+          <p>
+            基于复杂决策模型，进行态势感知、预测分析、仿真模拟等，实现对决策业务事前判断、事中控制、事后反馈的全局智能支持系统。
+          </p>
+        </div>      <div class="decision ">
+          <img src="../../assets/images/0dd34c5c-078a-4edc-a983-af8554dcb333.png" />
+          <h3>
+            DeepGovernor <br>
+            智能政府决策系统
+          </h3>
+          <p>
+            基于复杂决策模型，进行态势感知、预测分析、仿真模拟等，实现对决策业务事前判断、事中控制、事后反馈的全局智能支持系统。
+          </p>
+        </div>      <div class="decision ">
+          <img src="../../assets/images/0dd34c5c-078a-4edc-a983-af8554dcb333.png" />
+          <h3>
+            DeepGovernor <br>
+            智能政府决策系统
+          </h3>
+          <p>
+            基于复杂决策模型，进行态势感知、预测分析、仿真模拟等，实现对决策业务事前判断、事中控制、事后反馈的全局智能支持系统。
+          </p>
+        </div>      <div class="decision ">
+          <img src="../../assets/images/0dd34c5c-078a-4edc-a983-af8554dcb333.png" />
+          <h3>
+            DeepGovernor <br>
+            智能政府决策系统
+          </h3>
+          <p>
+            测分析、仿真模拟等，实现对决策业务事前判断、事中控制、事后反馈的全局智能支持系统。
+          </p>
+        </div>
+     </Col>
+   </Row>
+    <div class="bg_f5">
+    <Row>
+      <Col span="24" class="productTitle  bg_f5">
+      <h2>智能认知产品</h2>
+      <img src="../../assets/images/tips_img_M.png" height="11" width="99"/>
+      <p>通过动态知识图谱、模型工场、智能文本等系列智能工具，自动识别和判断分析业务问题，帮助行业客户构建智能认知基础能力。</p>
+      </Col>
+   </Row>
+    <Row>
+
+      <Col span="20" push="2" class="relize fle bg_f5">
+     <div class="decision">
+       <img src="../../assets/images/bbc2bd00-be45-4f94-8b54-7ccbcd3ef6a0.png" height="70" width="100"/>
+     <h3>智能交互式分析系统</h3>
+       <p>利用自然语言处理和语音识别技术，帮助企业进行智能数据提取、分析和可视化展示，智能引导企业数据分析的一站式数据管理工具。</p>
+     </div>
+      </Col>
+    </Row>
+    </div>
+    <div class="data mb30">
+    <Row>
+      <Col span="24" class="productTitle">
+      <h2>大数据操作系统</h2>
+      <img src="../../assets/images/tips_img_M.png" height="11" width="99"/>
+      <p>实时处理内外部多源、异构复杂海量数据，构建以数据生命周期管理为核心的数据资产管理平台。</p>
+      </Col>
+    </Row>
+    <Row>
+      <Col span="20" push="2" class="system fle ">
+      <div class="decision">
+        <img src="../../assets/images/36c25604-8f19-4a46-b86b-6b5543697a36.png" height="90" width="90"/>
+        <h3>分布式数据库 <br> KHan</h3>
+      </div>     <div class="decision">
+        <img src="../../assets/images/36c25604-8f19-4a46-b86b-6b5543697a36.png" height="90" width="90"/>
+        <h3>分布式数据库 <br> KHan</h3>
+      </div>     <div class="decision">
+        <img src="../../assets/images/36c25604-8f19-4a46-b86b-6b5543697a36.png" height="90" width="90"/>
+        <h3>分布式数据库 <br> KHan</h3>
+      </div>     <div class="decision">
+        <img src="../../assets/images/36c25604-8f19-4a46-b86b-6b5543697a36.png" height="90" width="90"/>
+        <h3>分布式数据库 <br> KHan</h3>
+      </div>     <div class="decision">
+        <img src="../../assets/images/36c25604-8f19-4a46-b86b-6b5543697a36.png" height="90" width="90"/>
+        <h3>分布式数据库 <br> KHan</h3>
+      </div>     <div class="decision">
+        <img src="../../assets/images/36c25604-8f19-4a46-b86b-6b5543697a36.png" height="90" width="90"/>
+        <h3>分布式数据库 <br> KHan</h3>
+      </div>
+      </Col>
+    </Row>
+
+    </div>
+    <div class="solution bg_f5 mb30">
+      <Row>
+        <Col span="24" class="productTitle">
+        <h2>解决方案</h2>
+        <img src="../../assets/images/tips_img_M.png" height="11" width="99"/>
+        <p>依托百分点一系列大数据及人工智能产品，为各行业打造智能场景整体解决方案，使企业能高效、便捷地进行数据资产管理和价值实现。</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="20" push="2">
+        <div class="decision">
+          <h1>银行</h1>
+          <div class="show">
+            <h1>银行</h1>
+            <Button type="ghost" class="detail">了解详情</Button>
+            <img src="../../assets/images/publicdown_img.png"/>
+            <div class="bg"></div>
+          </div>
+          <img src="../../assets/images/afe61011-b050-40cf-be25-7ac92a01c3bf.png"/>
+        </div>      <div class="decision">
+          <h1>银行</h1>
+          <div class="show">
+            <h1>银行</h1>
+            <Button type="ghost" class="detail">了解详情</Button>
+            <img src="../../assets/images/publicdown_img.png"/>
+            <div class="bg"></div>
+          </div>
+          <img src="../../assets/images/afe61011-b050-40cf-be25-7ac92a01c3bf.png"/>
+        </div>      <div class="decision">
+          <h1>银行</h1>
+          <div class="show">
+            <h1>银行</h1>
+            <Button type="ghost" class="detail">了解详情</Button>
+            <img src="../../assets/images/publicdown_img.png"/>
+            <div class="bg"></div>
+          </div>
+          <img src="../../assets/images/afe61011-b050-40cf-be25-7ac92a01c3bf.png"/>
+        </div>      <div class="decision">
+          <h1>银行</h1>
+          <div class="show">
+            <h1>银行</h1>
+            <Button type="ghost" class="detail">了解详情</Button>
+            <img src="../../assets/images/publicdown_img.png"/>
+            <div class="bg"></div>
+          </div>
+          <img src="../../assets/images/afe61011-b050-40cf-be25-7ac92a01c3bf.png"/>
+        </div>      <div class="decision">
+          <h1>银行</h1>
+          <div class="show">
+            <h1>银行</h1>
+            <Button type="ghost" class="detail">了解详情</Button>
+            <img src="../../assets/images/publicdown_img.png"/>
+            <div class="bg"></div>
+          </div>
+          <img src="../../assets/images/afe61011-b050-40cf-be25-7ac92a01c3bf.png"/>
+        </div>      <div class="decision">
+          <h1>银行</h1>
+          <div class="show">
+            <h1>银行</h1>
+            <Button type="ghost" class="detail">了解详情</Button>
+            <img src="../../assets/images/publicdown_img.png"/>
+            <div class="bg"></div>
+          </div>
+          <img src="../../assets/images/afe61011-b050-40cf-be25-7ac92a01c3bf.png"/>
+        </div>
+        </Col>
+      </Row>
+    </div>
+  </div>
   </template>
 <script>
 export default{
@@ -10,5 +197,163 @@ export default{
 }
 </script>
 <style scoped>
+.productTitle{
+  text-align: center;
+  line-height: 50px;
+  padding: 100px 0 50px 0;
+}
+  .productTitle h2{
+    font-size: 35px;
+    font-weight: normal;
+  }
+  .productTitle p{
+    color: #666;
+  }
+  .decision{
+    width: 360px;
+    height: 280px;
+    /*background-color: red;*/
+    float: left;
+    margin-top: 20px;
+padding: 0 20px 10px 20px;
+    text-align: center;
+  }
+  .decision:hover{
+    cursor: pointer;
+    box-shadow: 0px 0px 15px 2px #777;
+    transition: all .5s linear;
 
+  }
+.decision:hover h3{
+  color:#e21921;
+  }
+  .decision img{
+    width: 50%;
+    height: 50%;
+  }
+  .decision p,h3{
+    font-weight: normal;
+  margin-top: 10px;
+  }
+.relize .decision{
+  width: 320px;
+  height: 280px;
+  background-color: #ffffff ;
+  float: left;
+ margin: 10px 20px 10px 20px;
+  text-align: center;
+}
+.relize .decision img{
+  margin-top: 10px;
+  width: 100px;
+  height: 70px;
+}
+.relize .decision:hover{
+  box-shadow: 0px 0px 5px 1px #fffdef;
+  transition: all .5s linear;
+}
+  .fle{
+    display: flex;
+    justify-content:flex-start;
+    flex-wrap: wrap;
+  }
+  .data .system .decision{
+    width: 220px;
+    height: 220px;
+    border: 1px solid #666;
+    border-radius: 50%;
+    margin: 10px 20px 10px 20px;
+
+  }
+.data .system .decision img{
+  width: 90px;
+  height: 90px;
+  margin-top: 30px;
+}
+.data .system .decision p,h3{
+  font-weight: 700;
+  margin-top: 10px;
+}
+.data .system .decision:hover{
+  box-shadow: none;
+}
+ .solution .decision{
+   position: relative;
+  width: 250px;
+  height: 150px;
+   margin-right: 20px;
+  float: left;
+  margin-top: 20px;
+  padding: 0 20px 10px 20px;
+  text-align: center;
+   overflow: hidden;
+}
+.solution .decision:hover .show{
+  top: 0;
+
+}
+.solution .decision:hover>h1{
+display: none;
+
+}
+.solution .decision h1{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  font-size: 40px;
+  font-weight: normal;
+  color: white;
+  line-height: 180px;
+  left: 0;
+  z-index: 2;
+}
+.solution .decision .detail{
+margin: 0 auto;
+  margin-top: 80px;
+  color: white;
+}
+.solution .decision .show{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 180px;
+  font-size: 40px;
+  font-weight: normal;
+  color: white;
+  line-height: 180px;
+  left: 0;
+  z-index: 2;
+  transition: all .3s ease;
+}
+.solution .decision .show .bg{
+  width: 100%;
+  height: 100%;
+  opacity: .7;
+  background: #ff3b4d;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+.solution .decision .show h1{
+  line-height: normal;
+  top: 15%;
+}
+.solution .decision .show img{
+  width: auto;
+  height: auto;
+left: 50%;
+  margin-left: -77px;
+  top: 50%;
+
+}
+.solution .decision img{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height:100%;
+  z-index: 1;
+}
 </style>
--- src/components/solution/solution.vue
+++ src/components/solution/solution.vue
@@ -1,5 +1,59 @@
 <template>
-  <h2>  解决方案页</h2>
+  <div class="productTitle">
+    <img src="../../assets/images/product.png"/>
+    <div class="titleText">
+      <h1>银行解决方案</h1>
+      <p>为银行提供从基础设施建设到上层智能应用的全套解决方案，助力银行的差异化服务和个性化产品、产品创新、营销策略执行与全流程监控等业务。</p>
+      <p><Button type="error" size="large">立即咨询</Button></p>
+    </div>
+    <div class="solutionModule">
+      <p class="BjTitle">
+        方案背景
+      </p>
+      <p class="BjContent">
+        大数据时代银行面临着巨大的商业挑战：多方的竞争压力，市场业绩不佳，财务压力不断攀升，客户关系维护难度加深等。在互联网交易链条中，多种移动支付方式以及互联网金融正在不断冲击着银行的业务，这使得银行逐渐被边缘化和管道化。
+
+        面对多种商业挑战，借助大数据技术整合多方数据，银行可以通过深入洞察客户和市场，对客户制定个性化的挖、抢、保、拓策略，从而提升银行营销和风控效率，降低运营成本，并且更好的提升客户体验。
+      </p>
+    </div>
+    <div class="solutionModule">
+      <p class="BjTitle">
+        方案概述
+      </p>
+      <p class="GsContent">
+<span class="GsNer">        百分点基于八年来的大数据领域的理论研究和应用实践，为银行业客户提供端到端的整体解决方案，帮助银行实现海量多源异构数据的采集、整合，并运用大数据分析和数据挖掘技术，深入挖掘客户特征与需求。从而为银行的差异化服务和个性化产品、产品创新、营销策略执行与全流程监控等提供数据支撑。
+</span>
+        <span class="GsImg">
+
+      <img src="../../assets/images/fc176b01-4809-4dba-b85a-0c9b1033f223.png" /></span>
+      </p>
+    </div>
+    <div class="solutionModule">
+      <p class="BjTitle">应用场景</p>
+      <Tabs value="name1">
+        <TabPane label="精准营销" name="name1">政府部门可以通过系统内置的分要素和多状态人口预测模型，基于时间跨度、状态划分、区域范围等维度进行参数配置，输出人口预测结果，在制定人口、教育、医疗、就业等政策时，提供预测数据支持。通过系统还可预估不同政策方案对人口增长和人口数量的影响。根据不同方案中运行模型得到的预测结果，与政策期望达到的目标进行对比验证，辅助政府部门进行政策方案的制定和调整。</TabPane>
+        <TabPane label="智慧运营" name="name2">政府部门可以通过系统内置的分要素和多状态人口预测模型，基于时间跨度、状态划分、区域范围等维度进行参数配置，输出人口预测结果，在制定人口、教育、医疗、就业等政策时，提供预测数据支持。通过系统还可预估不同政策方案对人口增长和人口数量的影响。根据不同方案中运行模型得到的预测结果，与政策期望达到的目标进行对比验证，辅助政府部门进行政策方案的制定和调整。</TabPane>
+        <TabPane label="产品设计" name="name3">政府部门可以通过系统内置的分要素和多状态人口预测模型，基于时间跨度、状态划分、区域范围等维度进行参数配置，输出人口预测结果，在制定人口、教育、医疗、就业等政策时，提供预测数据支持。通过系统还可预估不同政策方案对人口增长和人口数量的影响。根据不同方案中运行模型得到的预测结果，与政策期望达到的目标进行对比验证，辅助政府部门进行政策方案的制定和调整。</TabPane>
+        <TabPane label="决策支持" name="name4">基于企业法人库、宏观经济数据库、税务部门自有数据库、纳税人行为数据等数据源，系统可以对企业数据、纳税人数据、税收数据进行宏观统计分析，预测税收数量及增长率。系统还可以对税源结构进行对比分析，挖掘潜在税源增长点，并能对有可能的税源流失下降风险进行预测和原因分析。</TabPane>
+      </Tabs>
+    </div>
+    <div class="solutionModule clearfix">
+      <p class="BjTitle">方案优势</p>
+      <div class="yS clearfix">
+        <div>
+          <img src="../../assets/images/3cee0044-4d39-4c11-ae1e-e7ac83cf7140.png" />
+        </div>
+        <div class="ysRight">
+          <p class="YsTitle">
+            成熟的数据资产管理体系
+          </p>
+          <p class="YsContent">
+            百分点自主研发的大数据资产管理平台是一套分布式的多工种协同作业的，管理大数据采集、加工和应用环境所有资源和任务的大数据操作系统，为银行构建新一代数据仓库，帮助银行管理数据资产并创造商业价值。大数据资产管理平台拥有基于分布式数据库的操作便捷性、数据生命周期管理的安全性、以及数据处理的稳定性和快捷性等特点。
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   export default {
@@ -11,6 +65,74 @@
     }
   }
 </script>
-<style >
-
+<style scoped>
+  .solutionModule .yS{
+    margin-right: 10px;
+    width: 48%;
+    margin-bottom: 10px;
+    float:left;
+  }
+  .YsTitle{
+    font-size: 20px;
+    color: #000000;
+    padding-bottom: 30px;
+  }
+  .ysRight{
+    width: 80%;
+    padding-left: 50px;
+  }
+  .solutionModule .yS div{
+    float: left;
+  }
+.GsImg{
+  display: inline-block;
+  width: 200px;
+  height: 100px;
+}
+.GsImg img{
+  width: 100%;
+  height: 100%;
+}
+.GsNer{
+  display: inline-block;
+  width: 70%;
+  margin-right: 100px;
+}
+  .solutionModule{
+    padding: 100px 100px 50px 100px;
+text-align: center;
+  }
+  .BjTitle{
+    font-size: 36px;
+    color: #000000;
+    padding-bottom: 30px;
+  }
+  .GsContent{
+    text-align: left;
+    width: 100%;
+    height: 200px;
+    border: 1px solid #0C172B;
+    padding: 30px;
+  }
+  .productTitle{
+    position: relative;
+  }
+  .productTitle>img{
+    width: 100%;
+  }
+  .titleText{
+    width: 100%;
+    position: absolute;
+    text-align: center;
+    top: 100px;
+    color: white;
+    padding: 15px 0;
+    background: rgba(0,0,0,.6);
+  }
+  .titleText h1{
+    font-size: 36px;
+  }
+  .titleText p{
+    margin-top: 20px;
+  }
 </style>
